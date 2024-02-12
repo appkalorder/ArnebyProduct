e2762a5f22f798488ffe982ba65b60c1b894b0a1
appkalorder <appkalorder@gmail.com>
2024-02-12 02:12:43 -0500

    Headers try 2

--- server/chunks/index_IlCJKWi9.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./prerender_Toooq3Zv.mjs').then(n => n.i);
-
-export { page };
--- server/chunks/prerender_Toooq3Zv.mjs
+++
@@ -1,5 +0,0 @@
-const noop = () => {};
-export const i = noop;
-export const l = noop;
-export const p = noop;
-export const s = noop;
