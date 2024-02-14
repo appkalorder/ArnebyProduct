854a01cfe0aa4ae462c613a438c3ed4862ce1856
esgmdigital <esgm.digital@gmail.com>
2024-02-14 14:05:44 -0500

    Astro add ssr

---
+++ dist/server/chunks/node_E_cM45ap.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/node_AT_vRIFk.mjs').then(n => n.n);
+
+export { page };
