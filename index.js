889e26766e0919fef11022eb640f654bb080702a
esgmdigital <esgm.digital@gmail.com>
2024-02-14 14:00:49 -0500

    astro allow ssr

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { handler as ssrHandler } from './dist/server/entry.mjs';
+
+const PORT = process.env.PORT ?? 3000;
+
+const app = express();
+// Change this based on your astro.config.mjs, `base` option.
+// They should match. The default value is "/".
+const base = '/';
+app.use(base, express.static('dist/client/'));
+console.log(`Server listening on port ${PORT}`);
+app.use(ssrHandler);
+
+app.listen(PORT);
