f085e0dfb13130a2c26524c0b29544ebf3215cfd
esgmdigital <esgm.digital@gmail.com>
2024-02-15 07:42:29 -0500

    fix port project

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
 import { handler as ssrHandler } from './dist/server/entry.mjs';
 
-const PORT = process.env.PORT ?? 2999;
+const PORT = process.env.PORT ?? 3000;
 
 const app = express();
-// Change this based on your astro.config.mjs, `base` option.
-// They should match. The default value is "/".
+app.disable('x-powered-by'); // Deshabilitar el encabezado "X-Powered-By"
+
+app.use(morgan('dev')); // Usar Morgan para registrar las solicitudes HTTP
+app.use(cookieParser()); // Usar Cookie Parser para trabajar con cookies
+
 const base = '/';
 app.use(base, express.static('dist/client/'));
-console.log(`Server listening on port ${PORT}`);
 app.use(ssrHandler);
 
 app.listen(PORT);
